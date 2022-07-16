71f2cfc4113a5d2487e05fab7b548b5aa8700e1b
Brann Joly <joly.brann@gmail.com>
2022-07-16 23:15:13 +0200

    end of lesson 12

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,8 +24,16 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, error, isError } = useQuery(
+    ["comments", post.id],
+    () => fetchComments(post.id),
+    {
+      staleTime: 2000,
+    }
+  );
+
+  if (isLoading) return <h3>loading</h3>;
+  if (isError) return <h3>{error.toString()}</h3>;
 
   return (
     <>
